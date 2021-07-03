1c2fe0079f2f6934169841c0c0cb07504a8da2db
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-07-03 16:31:02 -0400

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :accounts, only: [:show]
+  resources :users  #, only: [:new, :create]
+  # post '/' => 'sessions#create'
+  root 'site#welcome'
 end
