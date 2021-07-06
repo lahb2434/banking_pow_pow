f37d844e6f9d8095a4aea5e24c31d03034acb2f0
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-07-06 15:02:03 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :accounts, only: [:show]
-  resources :users  #, only: [:new, :create]
-  # post '/' => 'sessions#create'
+  resources :users, only: [:new, :create, :show] do
+    resources :accounts, only: [:new, :create, :show]
+  end
+  resources :accounts
+  resources :sessions, only: [:create]
+  get '/home', to: 'site#home'
+  get '/welcome', to: 'site#welcome'
   root 'site#welcome'
+
+  get '/users/auth/facebook/callback' => 'sessions#create'
+  # get '/auth/face/callback', to: 'sessions#create'
+
+  devise_for :users
 end
