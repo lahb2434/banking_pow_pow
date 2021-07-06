2fc8d1dffb6cde57f97296aa116e880a7f6be991
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-07-06 14:57:31 -0400

    User create method and private getter method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
 
-    def new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
+  def create
+    @user = User.new(user_params)
+    @user.admin = true if params[:user][:admin]=='1'
+    if @user.save
+        session[:user_id] = @user.id 
+      redirect_to @user
+    else
+      render :new
     end
+  end
+
+  def show
+  end
+  
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(
+      :first_name, 
+      :last_name, 
+      :user_name, 
+      :password, 
+      :password_confirmation, 
+      :email, 
+      :telephone_number, 
+      :birth_date, 
+      :admin)
+  end
     
 end
