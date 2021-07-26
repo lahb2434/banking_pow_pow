eae160d9a729dd1ea68503d2e86bbac404aa4752
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-07-26 00:37:22 -0400

    refactor store method in merch controller

--- app/helpers/merchandise_helper.rb
+++ app/helpers/merchandise_helper.rb
@@ -1,18 +1,6 @@
 module MerchandiseHelper
 
-    def store_greeting(product, price)
-      mack = content_tag :div, class: "merch_container" do 
-        content_tag :div, class: "merch_description" do
-          content_tag :p do  
-            "Buy a new #{product}"
-            "priced at only $ #{formatted_balance(@balance = price)}"
-          end
-        end
-      end
-      mack
-    end
-
-    def store_products
+    def store
       store = [
         {name: 'House', price: 200000},
         {name: 'Car', price: 40000},
@@ -20,8 +8,6 @@ module MerchandiseHelper
         {name: 'ATV', price: 15000},
         {name: 'Designer Dog Breed', price: 2500},
         {name: 'Candy Bar', price: 1}]
-
-      store.map{|product| store_greeting(product[:name], product[:price])}
     end
 
 end
